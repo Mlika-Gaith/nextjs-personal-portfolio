b52fdf031812ab031a24471593d8fcd159a6075f
ghaith_mlika <mlika.gaith@gmail.com>
2024-05-21 00:51:14 +0100

    Add diploma button

--- components/buttons/OutlineBtn.tsx
+++ components/buttons/OutlineBtn.tsx
@@ -1,17 +1,17 @@
-import {FC,MouseEventHandler, ReactNode} from "react";
+import { FC, MouseEventHandler, ReactNode } from "react";
 import styles from "./outlineBtn.module.css";
-import {BsFiletypePdf} from "react-icons/bs";
+import { BsFiletypePdf } from "react-icons/bs";
 
 type Props = {
-    children: string | ReactNode;
-    onClick: MouseEventHandler<HTMLButtonElement>
-}
+  children: string | ReactNode;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+};
 
-export const OutlineBtn:FC<Props> = ({children, onClick}) => {
-    return (
-        <button onClick={onClick} className={styles.outlineBtn}>
-            <BsFiletypePdf size="2.2rem" className={styles.svgBackground}/>
-            {children}
-        </button>
-    );
+export const OutlineBtn: FC<Props> = ({ children, onClick }) => {
+  return (
+    <button onClick={onClick} className={styles.outlineBtn}>
+      <BsFiletypePdf size="2.2rem" className={styles.svgBackground} />
+      {children}
+    </button>
+  );
 };
--- components/nav/HeadBar.tsx
+++ components/nav/HeadBar.tsx
@@ -1,17 +1,15 @@
-"use client"
-import styles from "./headbar.module.css"
-import {Links} from "@components/links"
-import {OutlineBtn} from "@components/buttons"
-
+"use client";
+import styles from "./headbar.module.css";
+import { Links } from "@components/links";
+import { OutlineBtn } from "@components/buttons";
 
 export const HeadBar = () => {
-    return (
-        <header className={styles.headBar}>
-            <Links/>
-            <OutlineBtn
-                onClick={() => window.open("/Ghaith.pdf")}>
-                My Resume
-            </OutlineBtn>
-        </header>
-    );
+  return (
+    <header className={styles.headBar}>
+      <Links />
+      <OutlineBtn onClick={() => window.open("/Ghaith.pdf")}>
+        My Resume
+      </OutlineBtn>
+    </header>
+  );
 };
