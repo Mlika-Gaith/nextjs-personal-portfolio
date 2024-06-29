86370e3ef3718a1a565708e95765d7d679c24d74
ghaith <mlika.gaith@gmail.com>
2024-06-29 13:41:27 +0100

    Style: update skills section style
    - Fix titles and paragraph colors
    - Fix Skill Chip background color

--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -1,8 +1,8 @@
 import { Reveal, SectionHeader } from "@utils";
-import { IoArrowForward } from "react-icons/io5";
 import { SkillsLinks } from "@components/links";
 import { SkillsChips } from "@components/skills/skills-chip";
 import styles from "./skills.module.css";
+import { TbArrowWaveRightUp } from "react-icons/tb";
 
 export const Skills = () => {
   return (
@@ -14,18 +14,21 @@ export const Skills = () => {
             <p className={styles.text}>
               During my university coursework in software engineering, I
               explored diverse technologies and frameworks. These valuable tools
-              have served as trusty allies, empowering me to craft both academic
-              and personal projects.
+              have served as trusty allies, empowering me to develop a wide
+              range of both academic and personal projects.
             </p>
           </Reveal>
           <Reveal>
             <p className={styles.text}>
               My skills revolve mainly around web development. Here, I mainly
-              use languages like JavaScript, TypeScript, Java, and Python. I’ve
-              also developed my skills using frontend frameworks like React.js
-              and Next.js, while also immersing myself in backend technologies
-              like Spring Boot, Express.js and FastAPI. Moreover, I possess some
-              experience working with databases such as PostgreSQL and MongoDB.
+              use languages like JavaScript, TypeScript, Java, and Python. When
+              it comes to frontend development, my framework of choice is
+              React.js. Since the create-react-app isn't the go-to anymore, I've
+              been getting more and more familiar with Next.js. For backend
+              development, I use Node.js and TypeScript because they're as
+              reliable as a backend dev saying, "It works on my machine." But
+              for better practices and more structured code, I also turn to Java
+              and Spring Boot.
             </p>
           </Reveal>
           <Reveal>
@@ -48,7 +51,7 @@ export const Skills = () => {
             <div className={styles.projectLinkWrapper}>
               <div className={styles.projectLink}>
                 <span>Check Projects</span>
-                <IoArrowForward />
+                <TbArrowWaveRightUp className="w-10 h-10" />
               </div>
               <SkillsLinks />
             </div>
