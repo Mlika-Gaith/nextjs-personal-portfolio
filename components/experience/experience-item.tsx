ff9242d5991abb5aececd4467eb91cab5457f0f6
ghaith <mlika.gaith@gmail.com>
2024-06-29 15:10:56 +0100

    Style: update experience section style
    - Update experience description text
    - Update experience section colors
    - Update project learn more link style in projects section

--- components/experience/experience-item.tsx
+++ components/experience/experience-item.tsx
@@ -1,56 +1,61 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import styles from "./experience.module.css";
-import {Reveal} from "@utils";
-
+import { Reveal } from "@utils";
+import Image from "next/image";
 type Props = {
-    title: string,
-    company: string,
-    time: string,
-    location: string,
-    description: string,
-    technologies: JSX.Element[]
-}
+  title: string;
+  company: string;
+  time: string;
+  location: string;
+  description: string;
+  technologies: JSX.Element[];
+};
 
 export const ExpItem: FC<Props> = ({
-                                       title,
-                                       company,
-                                       time,
-                                       location,
-                                       description,
-                                       technologies
-                                   }) => {
-    return (
-        <div className={styles.expItem}>
-            <div className={styles.heading}>
-                <Reveal>
-                    <span className={styles.title}>{title}</span>
-                </Reveal>
-                <Reveal>
-                    <span>{time}</span>
-                </Reveal>
-            </div>
+  title,
+  company,
+  time,
+  location,
+  description,
+  technologies,
+}) => {
+  return (
+    <div className={styles.expItem}>
+      <div className={styles.heading}>
+        <Reveal>
+          <span className={styles.title}>{title}</span>
+        </Reveal>
+        <Reveal>
+          <span className={styles.date}>{time}</span>
+        </Reveal>
+      </div>
 
-            <div className={styles.heading}>
-                <Reveal>
-                    <span className={styles.company}>{company}</span>
-                </Reveal>
-                <Reveal>
-                    <span>{location}</span>
-                </Reveal>
-            </div>
+      <div className={styles.heading}>
+        <Reveal>
+          <span className={styles.company}>{company}</span>
+          <Image
+            src="/assets/svg/line.svg"
+            width={50}
+            height={50}
+            alt="line"
+            className={styles.line}
+          />
+        </Reveal>
+        <Reveal>
+          <span className={styles.location}>{location}</span>
+        </Reveal>
+      </div>
 
-            <Reveal>
-                <p className={styles.description}>{description}</p>
-            </Reveal>
-            <Reveal>
-                <div className={styles.technologies}>
-                    {technologies.map((technology: JSX.Element, index) => (
-                        <div key={index}>
-                            {technology}
-                        </div>
-                    ))}
-                </div>
-            </Reveal>
+      <Reveal>
+        <p className={styles.description}>{description}</p>
+      </Reveal>
+      <Reveal>
+        <div className={styles.technologies}>
+          {technologies.map((technology: JSX.Element, index) => (
+            <div key={index}>{technology}</div>
+          ))}
         </div>
-    );
+      </Reveal>
+    </div>
+  );
 };
--- components/experience/experience.tsx
+++ components/experience/experience.tsx
@@ -5,7 +5,7 @@ import { ExpItem } from "@components/experience/experience-item";
 export const Experience = () => {
   return (
     <section className="section-wrapper" id="experience">
-      <SectionHeader title="Exp" direction="right" />
+      <SectionHeader title="Experience" direction="right" />
       {experiences.map((experience) => (
         <ExpItem key={experience.title} {...experience} />
       ))}
